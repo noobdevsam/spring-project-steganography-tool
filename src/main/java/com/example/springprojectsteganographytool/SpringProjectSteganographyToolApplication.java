8a2308fc394ae7301d3d5da537b6ff5f65ed6a83
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-28 00:22:06 +0600

    initial commit

---
+++ src/main/java/com/example/springprojectsteganographytool/SpringProjectSteganographyToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprojectsteganographytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectSteganographyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectSteganographyToolApplication.class, args);
+	}
+
+}
