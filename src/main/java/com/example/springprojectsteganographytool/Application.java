7642d28b5eb61b43cf5098e9f5dbb83a559b055e
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-28 00:27:05 +0600

    rename main application class and configuration file

--- src/main/java/com/example/springprojectsteganographytool/Application.java
+++ src/main/java/com/example/springprojectsteganographytool/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringProjectSteganographyToolApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringProjectSteganographyToolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
