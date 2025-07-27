c6a05f7c0611ab78ac2ba2bac2daa5bcac20323b
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-28 01:01:37 +0600

    add Jetty server configuration with virtual thread support

--- src/main/java/com/example/springprojectsteganographytool/Application.java
+++ src/main/java/com/example/springprojectsteganographytool/Application.java
@@ -2,12 +2,28 @@ package com.example.springprojectsteganographytool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public JettyServletWebServerFactory jettyFactory() {
+        var factory = new JettyServletWebServerFactory();
+        factory.addServerCustomizers(server -> {
+            server.setHandler(server.getHandler()); // Retain existing handler
+            server.addBean(
+                    Executors.newVirtualThreadPerTaskExecutor()
+            );
+        });
+        return factory;
     }
 
 }
