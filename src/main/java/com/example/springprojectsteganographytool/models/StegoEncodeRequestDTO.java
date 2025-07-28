9afdd0f00683dce472df8469afbf43e9a898c74f
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-28 19:51:49 +0600

    add comment to StegoEncodeRequestDTO for clarity on purpose

--- src/main/java/com/example/springprojectsteganographytool/models/StegoEncodeRequestDTO.java
+++ src/main/java/com/example/springprojectsteganographytool/models/StegoEncodeRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.springprojectsteganographytool.models;
 
 import org.springframework.web.multipart.MultipartFile;
 
+// used for encoding a message or file into a cover image
 public record StegoEncodeRequestDTO(
         MultipartFile coverImage,
         String textMessage,
