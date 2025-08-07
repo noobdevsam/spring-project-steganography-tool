785c403bf76ac5afd81c510d5a575f9abde25287
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-07 22:13:54 +0600

    refactor StegoEncodeRequestDTO to simplify fields for encoding requests

--- src/main/java/com/example/springprojectsteganographytool/models/StegoEncodeRequestDTO.java
+++ src/main/java/com/example/springprojectsteganographytool/models/StegoEncodeRequestDTO.java
@@ -5,10 +5,8 @@ import org.springframework.web.multipart.MultipartFile;
 // used for encoding a message or file into a cover image
 public record StegoEncodeRequestDTO(
         MultipartFile coverImage,
-        String textMessage,
-        MultipartFile fileToHide,
-        boolean encrypt,
-        String encryptionKey,
-        String passHint
+        MultipartFile embeddedFile,
+        String message,
+        String password
 ) {
 }
