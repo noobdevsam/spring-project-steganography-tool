87880908099c80c8eaab6456992e40d51a3998f3
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-07 19:38:30 +0600

    add Lombok annotations to StegoData for boilerplate code reduction

--- src/main/java/com/example/springprojectsteganographytool/documents/StegoData.java
+++ src/main/java/com/example/springprojectsteganographytool/documents/StegoData.java
@@ -1,11 +1,15 @@
 package com.example.springprojectsteganographytool.documents;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Document
+@Data
+@NoArgsConstructor
 public class StegoData {
 
     @Id
