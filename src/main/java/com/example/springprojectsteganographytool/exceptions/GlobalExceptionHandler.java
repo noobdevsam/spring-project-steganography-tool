61ec89e34fad989106c74a7aae86559fe85e795e
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-08 19:56:26 +0600

    add exception handlers for file-related exceptions in GlobalExceptionHandler

--- src/main/java/com/example/springprojectsteganographytool/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/springprojectsteganographytool/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.example.springprojectsteganographytool.exceptions;
 
+import com.example.springprojectsteganographytool.exceptions.file.FileTooLargeException;
+import com.example.springprojectsteganographytool.exceptions.file.FileTypeNotSupportedException;
+import com.example.springprojectsteganographytool.exceptions.file.InvalidImageFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
 import java.util.HashMap;
@@ -19,6 +23,21 @@ public class GlobalExceptionHandler {
         body.put("message", message);
 
         return new ResponseEntity<>(body, status);
+    }
+
+    @ExceptionHandler(FileTypeNotSupportedException.class)
+    public ResponseEntity<Object> handleFileTypeNotSupported(FileTypeNotSupportedException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FileTooLargeException.class)
+    public ResponseEntity<Object> handleFileTooLarge(FileTooLargeException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.PAYLOAD_TOO_LARGE);
+    }
+
+    @ExceptionHandler(InvalidImageFormatException.class)
+    public ResponseEntity<Object> handleInvalidImageFormat(InvalidImageFormatException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
