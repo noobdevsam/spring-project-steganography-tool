6ad726750cb7f72e7a4837d16abd022617b23861
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-08 18:37:05 +0600

    refactor AesUtilService interface to improve encryption and decryption methods

--- src/main/java/com/example/springprojectsteganographytool/services/AesUtilService.java
+++ src/main/java/com/example/springprojectsteganographytool/services/AesUtilService.java
@@ -1,36 +1,30 @@
 package com.example.springprojectsteganographytool.services;
 
-/**
- * Service interface for AES encryption and decryption operations.
- */
 public interface AesUtilService {
 
-    /**
-     * Encrypts the given plain text using the specified password.
-     * Returns: salt || iv || ciphertext
-     *
-     * @param plainText The byte array representing the plain text to encrypt.
-     * @param password  The password used for encryption.
-     * @return A byte array containing the encrypted data.
-     * @throws Exception If an error occurs during encryption.
-     */
-    byte[] encrypt(
-            byte[] plainText,
-            String password
+
+    byte[] encryptText(
+            String plainText,
+            String key
     ) throws Exception;
 
-    /**
-     * Decrypts the given input data using the specified password.
-     * Decrypts data formatted as: salt || iv || ciphertext
-     *
-     * @param input    The byte array representing the encrypted data to decrypt.
-     * @param password The password used for decryption.
-     * @return A byte array containing the decrypted data.
-     * @throws Exception If an error occurs during decryption.
-     */
-    byte[] decrypt(
-            byte[] input,
-            String password
+    String decryptText(
+            byte[] cipherBytes,
+            String key
+    ) throws Exception;
+
+    byte[] encryptFile(
+            byte[] fileBytes,
+            String key
+    ) throws Exception;
+
+    byte[] decryptFile(
+            byte[] cipherBytes,
+            String key
+    ) throws Exception;
+
+    String generateKey(
+            String key
     ) throws Exception;
 
 }
