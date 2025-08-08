cda930540883eb28282bb8636021e03162c53481
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-08 17:34:06 +0600

    refactor StegoDataRepository to return Optional for file name queries #4

--- src/main/java/com/example/springprojectsteganographytool/repos/StegoDataRepository.java
+++ src/main/java/com/example/springprojectsteganographytool/repos/StegoDataRepository.java
@@ -3,12 +3,13 @@ package com.example.springprojectsteganographytool.repos;
 import com.example.springprojectsteganographytool.documents.StegoData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StegoDataRepository extends MongoRepository<StegoData, UUID> {
 
-    StegoData findByOriginalFileName(String originalFileName);
+    Optional<StegoData> findByOriginalFileName(String originalFileName);
 
-    StegoData findByEmbeddedFileName(String embeddedFileName);
+    Optional<StegoData> findByEmbeddedFileName(String embeddedFileName);
 
 }
