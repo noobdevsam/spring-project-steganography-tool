5fff781ac9bfeefb180df7b390015d967b864152
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-08 17:51:33 +0600

    add methods for encoding and decoding in SteganographyService interface

--- src/main/java/com/example/springprojectsteganographytool/services/SteganographyService.java
+++ src/main/java/com/example/springprojectsteganographytool/services/SteganographyService.java
@@ -1,4 +1,53 @@
 package com.example.springprojectsteganographytool.services;
 
+import com.example.springprojectsteganographytool.models.StegoDecodeResponseDTO;
+import com.example.springprojectsteganographytool.models.StegoEncodeResponseDTO;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.UUID;
+
 public interface SteganographyService {
+
+    StegoEncodeResponseDTO encodeText(
+            BufferedImage coverImage,
+            String message,
+            String password,
+            int lsbDepth // 1 or 2 (bits per channel)
+    ) throws Exception;
+
+    StegoEncodeResponseDTO encodeFile(
+            BufferedImage coverImage,
+            String originalFileName,
+            byte[] fileBytes,
+            String password,
+            int lsbDepth // 1 or 2 (bits per channel)
+    ) throws Exception;
+
+    StegoDecodeResponseDTO decodeProcess(
+            BufferedImage stegoImage,
+            String password
+    ) throws Exception;
+
+    byte[] encodeTextToBytes(
+            BufferedImage coverImage,
+            String message,
+            String password,
+            int lsbDepth // 1 or 2 (bits per channel)
+    ) throws Exception;
+
+    byte[] encodeFileToBytes(
+            BufferedImage coverImage,
+            String originalFileName,
+            byte[] fileBytes,
+            String password,
+            int lsbDepth // 1 or 2 (bits per channel)
+    ) throws Exception;
+
+    List<StegoEncodeResponseDTO> listAllEncodings() throws Exception;
+
+    StegoEncodeResponseDTO getById(UUID id) throws Exception;
+
+    void deleteById(UUID id) throws Exception;
+
 }
