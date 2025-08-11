7e5bb73b5acea66f39d37e375009a526e00161b1
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-11 14:24:02 +0600

    implement LsbUtilService with methods for encoding and decoding messages and files

---
+++ src/main/java/com/example/springprojectsteganographytool/services/impl/LsbUtilServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.springprojectsteganographytool.services.impl;
+
+import com.example.springprojectsteganographytool.exceptions.data.MessageTooLargeException;
+import com.example.springprojectsteganographytool.exceptions.data.StegoDataNotFoundException;
+import com.example.springprojectsteganographytool.exceptions.file.InvalidImageFormatException;
+import com.example.springprojectsteganographytool.exceptions.lsb.InvalidLsbDepthException;
+import com.example.springprojectsteganographytool.exceptions.lsb.LsbDecodingException;
+import com.example.springprojectsteganographytool.exceptions.lsb.LsbEncodingException;
+import com.example.springprojectsteganographytool.exceptions.metadata.MetadataDecodingException;
+import com.example.springprojectsteganographytool.exceptions.metadata.MetadataNotFoundException;
+import com.example.springprojectsteganographytool.models.StegoMetadataDTO;
+import com.example.springprojectsteganographytool.services.LsbUtilService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LsbUtilServiceImpl implements LsbUtilService {
+
+    @Override
+    public byte[] encodeMessage(byte[] imageBytes, byte[] messageBytes, StegoMetadataDTO metadata) throws InvalidLsbDepthException, MessageTooLargeException, LsbEncodingException, InvalidImageFormatException {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] decodeMessage(byte[] stegoImageBytes, int lsbDepth) throws InvalidLsbDepthException, LsbDecodingException, StegoDataNotFoundException, InvalidImageFormatException {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] encodeFile(byte[] imageBytes, byte[] fileBytes, StegoMetadataDTO metadata) throws InvalidLsbDepthException, MessageTooLargeException, LsbEncodingException, InvalidImageFormatException {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] decodeFile(byte[] stegoImageBytes, int lsbDepth) throws InvalidLsbDepthException, LsbDecodingException, StegoDataNotFoundException, InvalidImageFormatException {
+        return new byte[0];
+    }
+
+    @Override
+    public StegoMetadataDTO extractMetadata(byte[] stegoImageBytes) throws MetadataNotFoundException, MetadataDecodingException, InvalidImageFormatException {
+        return null;
+    }
+
+}
