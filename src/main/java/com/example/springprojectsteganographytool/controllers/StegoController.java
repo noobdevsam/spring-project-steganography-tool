da57442610ffd2e86eb89045ba8b7df858619f7f
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-18 20:28:27 +0600

    feat: add StegoController for handling API requests in the Steganography tool

---
+++ src/main/java/com/example/springprojectsteganographytool/controllers/StegoController.java
@@ -0,0 +1,9 @@
+package com.example.springprojectsteganographytool.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/stego")
+public class StegoController {
+}
