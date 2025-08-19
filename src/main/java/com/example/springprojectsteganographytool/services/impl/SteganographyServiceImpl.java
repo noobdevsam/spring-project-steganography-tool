7741d3596cad096b8a151c75da3fdedec941baa7
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-19 17:36:20 +0600

    feat: improve transaction handling in encodeText method #22

--- src/main/java/com/example/springprojectsteganographytool/services/impl/SteganographyServiceImpl.java
+++ src/main/java/com/example/springprojectsteganographytool/services/impl/SteganographyServiceImpl.java
@@ -24,6 +24,7 @@ import com.example.springprojectsteganographytool.services.AesUtilService;
 import com.example.springprojectsteganographytool.services.LsbUtilService;
 import com.example.springprojectsteganographytool.services.SteganographyService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.imageio.ImageIO;
@@ -59,18 +60,18 @@ public class SteganographyServiceImpl implements SteganographyService {
     }
 
     @Transactional(
-        rollbackFor = {
-            InvalidLsbDepthException.class,
-            MessageTooLargeException.class,
-            InvalidEncryptionKeyException.class,
-            LsbEncodingException.class,
-            AesOperationException.class,
-            MetadataEncodingException.class,
-            StorageException.class,
-            ExecutionException.class,
-            InterruptedException.class
-        },
-        propagation = Propagation.REQUIRED
+            rollbackFor = {
+                    InvalidLsbDepthException.class,
+                    MessageTooLargeException.class,
+                    InvalidEncryptionKeyException.class,
+                    LsbEncodingException.class,
+                    AesOperationException.class,
+                    MetadataEncodingException.class,
+                    StorageException.class,
+                    ExecutionException.class,
+                    InterruptedException.class
+            },
+            propagation = Propagation.REQUIRED
     )
     @Override
     public StegoEncodeResponseDTO encodeText(BufferedImage coverImage, String message, String password, int lsbDepth) throws InvalidLsbDepthException, MessageTooLargeException, InvalidEncryptionKeyException, LsbEncodingException, AesOperationException, MetadataEncodingException, StorageException, ExecutionException, InterruptedException {
