d39bd314540f6634346bea4ced33d5eb1ea88eea
noobdevsam <mdshoykotislam05@gmail.com>
2025-08-19 14:09:55 +0600

    feat: update StegoData class to use JPA Id annotation for entity identification

--- src/main/java/com/example/springprojectsteganographytool/entities/StegoData.java
+++ src/main/java/com/example/springprojectsteganographytool/entities/StegoData.java
@@ -3,8 +3,8 @@ package com.example.springprojectsteganographytool.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 import java.time.Instant;
 import java.util.UUID;
