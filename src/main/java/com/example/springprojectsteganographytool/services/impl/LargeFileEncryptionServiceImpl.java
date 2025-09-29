18472ed2c626a304f436b05911e93c9c374bdc7e
noobdevsam <mdshoykotislam05@gmail.com>
2025-09-29 19:44:36 +0600

    feat: add password validation in encryptToTempFile method to ensure non-empty passwords

--- src/main/java/com/example/springprojectsteganographytool/services/impl/LargeFileEncryptionServiceImpl.java
+++ src/main/java/com/example/springprojectsteganographytool/services/impl/LargeFileEncryptionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.springprojectsteganographytool.services.impl;
 
+import com.example.springprojectsteganographytool.exceptions.encryption.AesKeyInvalidException;
 import com.example.springprojectsteganographytool.models.EncryptedTempFile;
 import com.example.springprojectsteganographytool.services.LargeFileEncryptionService;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,16 @@ public class LargeFileEncryptionServiceImpl implements LargeFileEncryptionServic
 
     @Override
     public EncryptedTempFile encryptToTempFile(InputStream plain, String password) throws Exception {
+
+        if (password == null || password.isBlank()) {
+            throw new AesKeyInvalidException("Password cannot be null or empty");
+        }
+
+        var salt = new byte[AesUtilServiceImpl.SALT_LENGTH];
+        var iv = new byte[AesUtilServiceImpl.IV_LENGTH];
+        AesUtilServiceImpl.RANDOM.nextBytes(salt);
+        AesUtilServiceImpl.RANDOM.nextBytes(iv);
+
         return null;
     }
 
