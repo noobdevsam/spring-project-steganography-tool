fa94d95adc6f64fec5a9a58f812f43d8e52fac6c
noobdevsam <mdshoykotislam05@gmail.com>
2025-09-29 19:46:35 +0600

    feat: implement encryption initialization in LargeFileEncryptionServiceImpl using derived key and IV

--- src/main/java/com/example/springprojectsteganographytool/services/impl/LargeFileEncryptionServiceImpl.java
+++ src/main/java/com/example/springprojectsteganographytool/services/impl/LargeFileEncryptionServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.springprojectsteganographytool.services.LargeFileEncryptionSe
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import java.io.InputStream;
 
 @Service
@@ -23,6 +25,11 @@ public class LargeFileEncryptionServiceImpl implements LargeFileEncryptionServic
         var iv = new byte[AesUtilServiceImpl.IV_LENGTH];
         AesUtilServiceImpl.RANDOM.nextBytes(salt);
         AesUtilServiceImpl.RANDOM.nextBytes(iv);
+
+        var key = AesUtilServiceImpl.deriveKey(password, salt);
+        var cipher = Cipher.getInstance(AesUtilServiceImpl.CIPHER_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+
 
         return null;
     }
