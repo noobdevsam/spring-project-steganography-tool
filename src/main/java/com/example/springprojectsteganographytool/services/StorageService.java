706c9f28ab1c189afd35f32c3764f8769e040d3b
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-02 16:06:31 +0600

    refactor: change resolve method return type from Path to void in StorageService

--- src/main/java/com/example/springprojectsteganographytool/services/StorageService.java
+++ src/main/java/com/example/springprojectsteganographytool/services/StorageService.java
@@ -5,7 +5,7 @@ import java.nio.file.Path;
 public interface StorageService {
     Path save(String relativeFileName, byte[] content) throws Exception;
 
-    Path resolve(String relativeFileName) throws Exception;
+    void resolve(String relativeFileName) throws Exception;
 
     // Returns true if the file was deleted, false if the file did not exist
     boolean delete(String relativeFileName) throws Exception;
