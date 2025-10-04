95faa105ad8de92abd1949e6cd10732e767fc907
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-04 12:07:39 +0600

    feat: add StegoErrorCode enum for standardized error handling

---
+++ src/main/java/com/example/springprojectsteganographytool/exceptions/StegoErrorCode.java
@@ -0,0 +1,22 @@
+package com.example.springprojectsteganographytool.exceptions;
+
+public enum StegoErrorCode {
+
+    VALIDATION_ERROR,
+    PAYLOAD_TOO_LARGE,
+    INVALID_LSB_DEPTH,
+    INVALID_IMAGE_FORMAT,
+    ENCRYPTION_KEY_INVALID,
+    ENCRYPTION_PROCESS_ERROR,
+    METADATA_NOT_FOUND,
+    STEGO_DATA_NOT_FOUND,
+    STORAGE_ERROR,
+    OPERATION_NOT_ALLOWED,
+    DECODE_FAILURE,
+    INTERNAL_ERROR;
+
+    public String typeURI() {
+        return "https://api.example.com/errors/" + name();
+    }
+
+}
