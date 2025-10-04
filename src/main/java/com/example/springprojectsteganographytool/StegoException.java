2cb62b2939e00a5e99536c7e411d0cf8577632e8
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-04 12:11:04 +0600

    feat: introduce abstract StegoException class for standardized exception handling

---
+++ src/main/java/com/example/springprojectsteganographytool/StegoException.java
@@ -0,0 +1,18 @@
+package com.example.springprojectsteganographytool;
+
+import com.example.springprojectsteganographytool.exceptions.StegoErrorCode;
+import org.springframework.http.HttpStatus;
+
+public abstract class StegoException extends RuntimeException {
+    protected StegoException(String message) {
+        super(message);
+    }
+
+    protected StegoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public abstract StegoErrorCode code();
+
+    public abstract HttpStatus status();
+}
