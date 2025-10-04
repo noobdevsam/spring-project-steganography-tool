99c62fb120fdfdb9fd54060b466433e7e98cc869
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-04 12:54:18 +0600

    feat: refactor InvalidImageFormatException to extend StegoException with standardized error code and HTTP status

--- src/main/java/com/example/springprojectsteganographytool/exceptions/file/InvalidImageFormatException.java
+++ src/main/java/com/example/springprojectsteganographytool/exceptions/file/InvalidImageFormatException.java
@@ -1,29 +1,27 @@
 package com.example.springprojectsteganographytool.exceptions.file;
 
-/**
- * Exception thrown when an invalid image format is encountered.
- * This exception extends the {@link RuntimeException}, making it an unchecked exception.
- */
-public class InvalidImageFormatException extends RuntimeException {
+import com.example.springprojectsteganographytool.exceptions.StegoErrorCode;
+import com.example.springprojectsteganographytool.exceptions.StegoException;
+import org.springframework.http.HttpStatus;
 
-    /**
-     * Constructs a new InvalidImageFormatException with the specified detail message.
-     *
-     * @param message the detail message explaining the reason for the exception
-     */
+public class InvalidImageFormatException extends StegoException {
+
     public InvalidImageFormatException(String message) {
         super(message);
     }
 
-    /**
-     * Constructs a new InvalidImageFormatException with the specified detail message
-     * and cause.
-     *
-     * @param message the detail message explaining the reason for the exception
-     * @param cause   the cause of the exception (a throwable that caused this exception)
-     */
     public InvalidImageFormatException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    @Override
+    public StegoErrorCode code() {
+        return StegoErrorCode.INVALID_IMAGE_FORMAT;
+    }
+
+    @Override
+    public HttpStatus status() {
+        return HttpStatus.UNSUPPORTED_MEDIA_TYPE;
     }
 
 }
